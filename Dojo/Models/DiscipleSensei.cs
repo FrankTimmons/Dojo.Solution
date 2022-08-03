9589397bad0ff41b918e2bbe408c75500708004a
FrankTimmons <franktimmonspdx@gmail.com>
2022-08-03 10:36:18 -0700

    Add stylesheet and layout file.
    Co-authored-by: Alex Shevlin <alexshevlin1@gmail.com>

--- Dojo/Models/DiscipleSensei.cs
+++ Dojo/Models/DiscipleSensei.cs
@@ -7,7 +7,6 @@ namespace Dojo.Models
     public int DiscipleSenseiId { get; set; }
     public int DiscipleId { get; set; }
     public int SenseiId { get; set; }
-
     public virtual Disciple Disciple { get; set; }
     public virtual Sensei Sensei { get; set; }
   }
