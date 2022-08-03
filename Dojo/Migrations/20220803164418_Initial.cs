a29f6d83317b9f824cc903ddf3f21ff676e48ae1
FrankTimmons <franktimmonspdx@gmail.com>
2022-08-03 09:52:57 -0700

    Add migrations to construct database.
    Co-authored-by: Alex Shevlin <a-shevlin@users.noreply.github.com>

---
+++ Dojo/Migrations/20220803164418_Initial.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dojo.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Disciples",
+                columns: table => new
+                {
+                    DiscipleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Disciples", x => x.DiscipleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MartialArts",
+                columns: table => new
+                {
+                    MartialArtId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Type = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MartialArts", x => x.MartialArtId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Senseis",
+                columns: table => new
+                {
+                    SenseiId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    MartialArtId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Senseis", x => x.SenseiId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DiscipleSensei",
+                columns: table => new
+                {
+                    DiscipleSenseiId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DiscipleId = table.Column<int>(type: "int", nullable: false),
+                    SensieId = table.Column<int>(type: "int", nullable: false),
+                    SenseiId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiscipleSensei", x => x.DiscipleSenseiId);
+                    table.ForeignKey(
+                        name: "FK_DiscipleSensei_Disciples_DiscipleId",
+                        column: x => x.DiscipleId,
+                        principalTable: "Disciples",
+                        principalColumn: "DiscipleId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DiscipleSensei_Senseis_SenseiId",
+                        column: x => x.SenseiId,
+                        principalTable: "Senseis",
+                        principalColumn: "SenseiId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiscipleSensei_DiscipleId",
+                table: "DiscipleSensei",
+                column: "DiscipleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiscipleSensei_SenseiId",
+                table: "DiscipleSensei",
+                column: "SenseiId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiscipleSensei");
+
+            migrationBuilder.DropTable(
+                name: "MartialArts");
+
+            migrationBuilder.DropTable(
+                name: "Disciples");
+
+            migrationBuilder.DropTable(
+                name: "Senseis");
+        }
+    }
+}
---
+++ Dojo/Migrations/20220803164940_CorrectSpellingForSenseiId.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dojo.Migrations
+{
+    public partial class CorrectSpellingForSenseiId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DiscipleSensei_Senseis_SenseiId",
+                table: "DiscipleSensei");
+
+            migrationBuilder.DropColumn(
+                name: "SensieId",
+                table: "DiscipleSensei");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SenseiId",
+                table: "DiscipleSensei",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DiscipleSensei_Senseis_SenseiId",
+                table: "DiscipleSensei",
+                column: "SenseiId",
+                principalTable: "Senseis",
+                principalColumn: "SenseiId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DiscipleSensei_Senseis_SenseiId",
+                table: "DiscipleSensei");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SenseiId",
+                table: "DiscipleSensei",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SensieId",
+                table: "DiscipleSensei",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DiscipleSensei_Senseis_SenseiId",
+                table: "DiscipleSensei",
+                column: "SenseiId",
+                principalTable: "Senseis",
+                principalColumn: "SenseiId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
