a0ab860aba7722ee8a9b3e4c387ce5912c6be224
FrankTimmons <franktimmonspdx@gmail.com>
2022-08-03 09:55:58 -0700

    Add MartialArt model.
    Co-authored-by: Alex Shevlin <a-shevlin@users.noreply.github.com>

---
+++ Dojo/Models/MartialArt.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System;
+
+namespace Dojo.Models
+{
+  public class MartialArt
+  {
+    public MartialArt()
+    {
+
+    }
+    public int MartialArtId { get; set; }
+    public string Type { get; set; }
+  }
+}
